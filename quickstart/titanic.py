6153a2b866e8a76e29ab18c6659e906fea406ddd
Ettore Delprino <edelprino@gmail.com>
2019-10-23 18:14:01 +0200

    Commit

--- quickstart/titanic.py
+++ quickstart/titanic.py
@@ -1,17 +1,17 @@
 from __future__ import print_function
-
 import numpy as np
 import tflearn
+import pandas as pd
 from tflearn.datasets import titanic
 from tflearn.data_utils import load_csv
 
-# Download the Titanic dataset
-titanic.download_dataset('titanic_dataset.csv')
+filename = 'titanic_dataset.csv'
+titanic.download_dataset(filename)
 
-import pandas as pd
-dataset = pd.read_csv('titanic_dataset.csv')
+dataset = pd.read_csv(filename)
 dataset = dataset.drop(columns='name').drop(columns='ticket')
 dataset['sex'] = dataset['sex'].apply(lambda sex: 1 if sex == 'female' else 0)
+
 labels = pd.get_dummies(dataset.pop('survived')).values
 data = dataset.values
 
@@ -21,17 +21,14 @@ net = tflearn.fully_connected(net, 32)
 net = tflearn.fully_connected(net, 32)
 net = tflearn.fully_connected(net, 2, activation='softmax')
 net = tflearn.regression(net)
-
-# Define model
 model = tflearn.DNN(net)
+
 # Start training (apply gradient descent algorithm)
 model.fit(data, labels, n_epoch=20, batch_size=16, show_metric=True)
 
 # Let's create some data for DiCaprio and Winslet
 dicaprio = [3, 0, 19, 0, 0, 5.0000]
 winslet = [1, 1, 17, 1, 2, 100.0000]
-# Preprocess data
-# dicaprio, winslet = preprocess([dicaprio, winslet], to_ignore)
 # Predict surviving chances (class 1 results)
 pred = model.predict([dicaprio, winslet])
 print("DiCaprio Surviving Rate:", pred[0][1])
