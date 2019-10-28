47c6f107e96c5df990fa5418489ece4486348733
Ettore Delprino <edelprino@gmail.com>
2019-10-28 16:08:51 +0100

    Added OR ML model

--- quickstart/or_operator.py
+++ quickstart/or_operator.py
@@ -8,10 +8,11 @@ neural_net = tflearn.fully_connected(neural_net, 1, activation='sigmoid')
 neural_net = tflearn.regression(neural_net, optimizer='sgd', learning_rate=2, loss='mean_square')
 
 model = tflearn.DNN(neural_net)
-model.fit(OR, Y_truth, n_epoch=2000, snapshot_epoch=False)
+model.fit(OR, Y_truth, n_epoch=2000, snapshot_epoch=False, show_metric=True)
 
 print("Testing OR operator")
 print("0 or 0:", model.predict([[0., 0.]]))
 print("0 or 1:", model.predict([[0., 1.]]))
 print("1 or 0:", model.predict([[1., 0.]]))
 print("1 or 1:", model.predict([[1., 1.]]))
+
